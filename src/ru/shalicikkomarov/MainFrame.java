0aff2d05c375c9057722e1fea872624411f4ed79
Shady <shady@shady-Aspire-5741G.(none)>
2013-04-21 17:06:42 +0400

    move

--- src/ru/shalicikkomarov/MainFrame.java
+++ src/ru/shalicikkomarov/MainFrame.java
@@ -2,6 +2,8 @@ package ru.shalicikkomarov;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 
 public class MainFrame extends JFrame {
@@ -13,6 +15,7 @@ public class MainFrame extends JFrame {
 		root = getContentPane();
         snakeField = new SnakeField();
         root.add(snakeField);
+        this.addKeyListener(new EventKey());
         this.pack();
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -31,6 +34,38 @@ public class MainFrame extends JFrame {
             }
         });
     }
+	class EventKey implements KeyListener {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			int key = e.getKeyCode();
+			SnakeElem elem = Snake.sl.get(0);
+			switch (key) {
+				case KeyEvent.VK_UP:
+					if(elem.z != 2) elem.z = 0;
+					break;
+				case KeyEvent.VK_DOWN:
+					if(elem.z != 0) elem.z = 2;
+					break;
+				case KeyEvent.VK_LEFT:
+					if(elem.z != 1) elem.z = 3;
+					break;
+				case KeyEvent.VK_RIGHT:
+					if(elem.z != 3) elem.z = 1;
+					break;
+				default:
+					break;
+			}
+			Snake.sl.set(0, elem);
+		}
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+		}
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+		}
+	}
     
     class SnakeThread implements Runnable{
         @Override
--- src/ru/shalicikkomarov/Snake.java
+++ src/ru/shalicikkomarov/Snake.java
@@ -3,19 +3,55 @@ package ru.shalicikkomarov;
 import java.awt.*;
 import java.util.ArrayList;
 
-
 public class Snake {
-	ArrayList<SnakeElem> sl = new ArrayList<SnakeElem>();
+	static ArrayList<SnakeElem> sl = new ArrayList<SnakeElem>();
 	SnakeElem e;
+	private long nowTime = 0, lastTime = 0;
 	public Snake() {
 		sl.add(new SnakeElem(1, 0, 1));
 		sl.add(new SnakeElem(0, 0, 1));
 	}
 	public void render(Graphics g, SnakeField io) {
+		nowTime = System.currentTimeMillis();
 		g.setColor(Color.RED);
 		for(int i = 0; i < sl.size(); i++) {
 			e = sl.get(i);
 			g.drawOval(e.x * Cnst.FCELL, e.y * Cnst.FCELL, Cnst.FCELL, Cnst.FCELL);
+		}
+		if(nowTime - lastTime > 200) {
+			move();
+			lastTime = nowTime;
+		}
+	}
+	private void move() {
+		for(int i = 0; i < sl.size(); i++) {
+			e = sl.get(i);
+			switch(e.z) {
+				case 0:
+					e.y--;
+					break;
+				case 1:
+					e.x++;
+					break;
+				case 2:
+					e.y++;
+					break;
+				case 3: 
+					e.x--;
+					break;
+				default: 
+					break;
+			}
+			if(e.x < 0) e.x = 15;
+			if(e.y < 0) e.y = 11;
+			e.x %= 16;
+			e.y %= 12;
+			sl.set(i, e);
+		}
+		for(int i = sl.size() - 1; i > 0; i--) {
+			e = sl.get(i);
+			e.z = sl.get(i - 1).z;
+			sl.set(i, e);
 		}
 	}
 }
